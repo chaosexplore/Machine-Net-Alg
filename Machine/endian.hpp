751797887a68fa71c861faba76feb677da907696
chaos <songcom@live.com>
2022-11-17 17:39:45 +0800

    add endian convert

--- Machine/endian.hpp
+++ Machine/endian.hpp
@@ -11,4 +11,11 @@ bool isBigEndian() {
     return c != 0x34;
 }
 
+#define Big2Little16(A) ((( (uint16_t)(A) & 0xff00) >> 8) | ( ((uint16_t)(A) & 0x00ff) << 8))
+
+#define Big2Little32(A) ((( (uint32_t)(A) & 0xff000000) >> 24) | \
+                        (( (uint32_t)(A) & 0x00ff0000 ) >> 8) | \
+                        (( (uint32_t)(A) & 0x0000ff00 ) << 8) | \
+                        (( (uint32_t)(A) & 0x000000ff ) << 24))
+
 #endif
